2be5a6063fba9cde25dd682841d6c0ebd082efec
Taisa Lima <taisalima300@gmail.com>
2025-10-07 11:41:16 -0300

    task 1

--- src/main.c
+++ src/main.c
@@ -1,10 +1,24 @@
-#include <zephyr/kernel.h>
+#include <zephyr.h>
 #include <zephyr/sys/printk.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/pwm.h>
+#include <zephyr/logging/log.h>
 
+#define INTERVAL_MS 10000  // Intervalo em milissegundos
 
-int main(void)
+LOG_MODULE_REGISTER(main);
+
+void timer_handler(struct k_timer *dummy) {
+    LOG_INF("Hello world com timer de %d ms", INTERVAL_MS);
+    LOG_DBG("Hello world com timer de %d ms", INTERVAL_MS);
+    LOG_ERR("Hello world com timer de %d ms", INTERVAL_MS);
+}
+
+// Define um timer
+K_TIMER_DEFINE(my_timer, timer_handler, NULL);
+
+void main(void)
 {
-    return 0;
+    LOG_INF("Sistema inicializado. Intervalo do timer: %d ms", INTERVAL_MS);
+
+    // Inicializa o timer para disparar periodicamente
+    k_timer_start(&my_timer, K_MSEC(INTERVAL_MS), K_MSEC(INTERVAL_MS));
 }
